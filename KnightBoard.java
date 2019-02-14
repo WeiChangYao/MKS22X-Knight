d30bf69d4d243dc14669ecd226e6eac8691aae67
weichangyao <wyao1@stuy.edu>
2019-02-14 11:33:33 -0500

    fixed constructor'

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,6 +1,11 @@
 public class KnightBoard{
 	private int[][] board;
-	public KnightBoard(int x, int y){
-
+	public KnightBoard(int r, int c){
+		board = new int[r][c];
+		for (int i = 0; i < r; i++){
+			for (int j = 0; j < c; j++){
+				board[i][j] = 0;
+			}
 		}
+	}
 }
